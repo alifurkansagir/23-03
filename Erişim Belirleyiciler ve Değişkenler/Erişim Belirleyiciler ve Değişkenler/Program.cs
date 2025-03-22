16b3292a96e73c7a09b2d537a9697d9770951ef5
alifurkansagir <alifurkan309@gmail.com>
2025-03-22 17:49:21 +0300

    Add project files.

---
+++ Erişim Belirleyiciler ve Değişkenler/Erişim Belirleyiciler ve Değişkenler/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KullaniciYonetimi
+{
+    public class User
+    {
+        private string isim;
+        private string soyisim;
+        private int yas;
+        public string Email { get; set; }
+
+        public string GetIsim()
+        {
+            return isim;
+        }
+
+        public void SetIsim(string isim)
+        {
+            this.isim = isim;
+        }
+
+        public string GetSoyisim()
+        {
+            return soyisim;
+        }
+
+        public void SetSoyisim(string soyisim)
+        {
+            this.soyisim = soyisim;
+        }
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int age)
+        {
+            if (age > 0)
+                yas = age;
+            else
+                Console.WriteLine("Yas negatif olamaz!");
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Isim: {isim}\n" +
+                $"Soyisim: {soyisim}\n" +
+                $"Yas: {yas}\n" +
+                $"Email: {Email}");
+        }
+    }
+}
+
+namespace Odev_ErisimBelirleyicilerVeDegiskenler
+{
+    using KullaniciYonetimi;
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            User kullanici = new User();
+            kullanici.SetIsim("Ali furkan");
+            kullanici.SetSoyisim("Sağır");
+            kullanici.SetYas(22);
+            kullanici.Email = "alifurkan309@gmail.com";
+            kullanici.BilgileriGoster();
+        }
+    }
+}
